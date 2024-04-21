2efd90edf6791469489ffd5c8848cc3d5c863eef
Jacob Hensley <jacobdlhensley@gmail.com>
2024-04-21 16:33:54 -0500

    Finished act 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(name: 'Toy Laser Gun', price: 10, description: 'It shoots lasers.' )
+Product.create!(name: 'Building Blocks', price: 5, description: 'You can build with them.' )
+Product.create!(name: 'Doll', price: 7, description: 'A doll that speaks.' )
